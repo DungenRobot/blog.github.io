134e790cf3c4dc484f182361a91640540de831e4
DungenRobot <104450078+DungenRobot@users.noreply.github.com>
2023-05-14 22:03:10 -0400

    Made it look better
    Changed Jekyll theme
    Added favicon
    Modified the paginator display

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Md Testing",
+        "excerpt":"&lt;script&gt; this is code &lt;/script&gt;   Note: This is a notice test1                    This is an annotation test &#8617;           ","categories": [],
+        "tags": [],
+        "url": "/fuckaround/findout",
+        "teaser": null
+      },{
+        "title": "Jam Entries",
+        "excerpt":"About a month ago I hosted a jam for myself and some friends of mine. Anyone could suggest and vote on the theme. Once a theme was selected, everyone would have ten days to submit something based around it. The Jam’s theme was “Sun and Moon” The entries are in...","categories": [],
+        "tags": [],
+        "url": "/jam/01",
+        "teaser": null
+      }]
