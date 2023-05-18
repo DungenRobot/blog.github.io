723b73015820be22fce0fe3b802f57bf85d12aab
DungenRobot <104450078+DungenRobot@users.noreply.github.com>
2023-05-17 23:34:23 -0400

    Uploaded winners

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
-        "title": "Md Testing",
-        "excerpt":"&lt;script&gt; this is code &lt;/script&gt;   Note: This is a notice test1                    This is an annotation test &#8617;           ","categories": [],
-        "tags": [],
-        "url": "/fuckaround/findout",
-        "teaser": null
-      },{
         "title": "Jam Entries",
         "excerpt":"About a month ago I hosted a jam for myself and some friends of mine. Anyone could suggest and vote on the theme. Once a theme was selected, everyone would have ten days to submit something based around it. The Jam’s theme was “Sun and Moon” The entries are in...","categories": [],
         "tags": [],
         "url": "/jam/01",
+        "teaser": null
+      },{
+        "title": "I like your thinking",
+        "excerpt":"You’re almost looking in the right place    Try again   ","categories": [],
+        "tags": [],
+        "url": "/HIDDEN/",
+        "teaser": null
+      },{
+        "title": "Category Winners!",
+        "excerpt":"Howdy ya’ll! I’m back to reveal the winners of our three categories! Excellent Aestetics Our first winner is... Sun and Moon in the Sky by @Teagodess224 \"Sun and moon and stars take over the skies\" - @Teagodess224 Most Impressive The entry that just blew me away with the amount of...","categories": [],
+        "tags": [],
+        "url": "/sWRUGBiM15CoHmBU6/categories-revealed",
         "teaser": null
       }]
